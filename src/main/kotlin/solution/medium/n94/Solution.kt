fc3c209107c3359b8ea43daf127ddc424594dad2
Alexander Shkabara <alexander.shkabara@reltio.com>
2020-06-13 15:36:48 +0500

    94. Binary Tree Inorder Traversal

---
+++ src/main/kotlin/solution/medium/n94/Solution.kt
@@ -0,0 +1,25 @@
+package solution.medium.n94
+
+import solution.util.TreeNode
+
+/**
+ * 94. Binary Tree Inorder Traversal
+ *
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ */
+class Solution {
+    fun inorderTraversal(root: TreeNode?): List<Int> {
+        val result = ArrayList<Int>()
+        inorderTraversal(root, result)
+        return result
+    }
+
+    private fun inorderTraversal(root: TreeNode?, result: MutableList<Int>) {
+        if (root == null) {
+            return
+        }
+        root.left?.let { left -> inorderTraversal(left, result) }
+        result.add(root.`val`)
+        root.right?.let { right -> inorderTraversal(right, result) }
+    }
+}
