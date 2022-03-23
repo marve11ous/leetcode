fbb761155c99ae112867d1e2f459d6eddb4060be
AShkabara <shkabara@dev.vtb.ru>
2022-03-23 16:24:53 +0300

    991. Broken Calculator

---
+++ src/test/kotlin/solution/medium/n991/SolutionTest.kt
@@ -0,0 +1,27 @@
+package solution.medium.n991
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+/**
+ * @author AShkabara
+ */
+internal class SolutionTest {
+    private val solution = Solution()
+
+    @Test
+    fun test1() {
+        assertEquals(2, solution.brokenCalc(startValue = 2, target = 3))
+    }
+
+    @Test
+    fun test2() {
+        assertEquals(2, solution.brokenCalc(startValue = 5, target = 8))
+    }
+
+    @Test
+    fun test3() {
+        assertEquals(3, solution.brokenCalc(startValue = 3, target = 10))
+    }
+}
