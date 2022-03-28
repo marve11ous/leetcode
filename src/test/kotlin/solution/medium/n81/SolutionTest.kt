6394d1f37dc061b7cf4cfc2b7a2cedaa0081ac8a
AShkabara <shkabara@dev.vtb.ru>
2022-03-28 11:03:06 +0500

    81. Search in Rotated Sorted Array II

---
+++ src/test/kotlin/solution/medium/n81/SolutionTest.kt
@@ -0,0 +1,28 @@
+package solution.medium.n81
+
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+/**
+ * @author AShkabara
+ */
+internal class SolutionTest {
+    val solution = Solution()
+
+    @Test
+    fun test1() {
+        assertTrue(solution.search(nums = intArrayOf(2, 5, 6, 0, 0, 1, 2), target = 0))
+    }
+
+    @Test
+    fun test2() {
+        assertFalse(solution.search(nums = intArrayOf(2, 5, 6, 0, 0, 1, 2), target = 3))
+    }
+
+    @Test
+    fun test3() {
+        assertTrue(solution.search(nums = intArrayOf(1, 0, 1, 1, 1), target = 0))
+    }
+
+}
