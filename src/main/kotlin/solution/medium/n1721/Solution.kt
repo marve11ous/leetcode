95dc8a41355475b598ff67e9a9aa6489bcc32888
ashkabara <shkabara@dev.vtb.ru>
2022-04-05 07:34:00 +0300

    1721. Swapping Nodes in a Linked List

---
+++ src/main/kotlin/solution/medium/n1721/Solution.kt
@@ -0,0 +1,40 @@
+package solution.medium.n1721
+
+/**
+ * [1721. Swapping Nodes in a Linked List](https://leetcode.com/problems/swapping-nodes-in-a-linked-list/)
+ */
+class Solution {
+    fun swapNodes(head: ListNode?, k: Int): ListNode? {
+        val nullNode = ListNode(-1).also { it.next = head }
+        val node1 = nullNode.next(k - 1)
+        var node2: ListNode? = nullNode
+        var current: ListNode? = nullNode.next(k + 1)
+        while (current != null) {
+            current = current.next
+            node2 = node2?.next
+        }
+        if (node1 != node2) {
+            swapLinks(node1, node2)
+            swapLinks(node1?.next, node2?.next)
+        }
+        return nullNode.next
+    }
+}
+
+private fun swapLinks(node1: ListNode?, node2: ListNode?) {
+    val temp = node1?.next
+    node1?.next = node2?.next
+    node2?.next = temp
+}
+
+private fun ListNode?.next(skip: Int): ListNode? {
+    var listNode: ListNode? = this
+    for (i in 0 until skip) {
+        listNode = listNode?.next
+    }
+    return listNode
+}
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
