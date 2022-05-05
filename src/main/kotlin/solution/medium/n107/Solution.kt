dbcace210331293d375ecec107df275b08fe69de
marve11ous <marve11ous@users.noreply.github.com>
2022-05-05 11:22:16 +0300

    107. Binary Tree Level Order Traversal II

---
+++ src/main/kotlin/solution/medium/n107/Solution.kt
@@ -0,0 +1,29 @@
+package solution.medium.n107
+
+import java.util.LinkedList
+
+/**
+ * [107. Binary Tree Level Order Traversal II](https://leetcode.com/problems/binary-tree-level-order-traversal-ii/)
+ */
+class Solution {
+    fun levelOrderBottom(root: TreeNode?): List<List<Int>> {
+        val result: LinkedList<List<Int>> = LinkedList()
+        val queue = LinkedList<TreeNode>().apply { root?.let { this.offer(it) } }
+        while (queue.isNotEmpty()) {
+            val list = ArrayList<Int>()
+            for (i in 1..queue.size) {
+                val node = queue.pop()
+                list.add(node.`val`)
+                node.left?.let { queue.offer(it) }
+                node.right?.let { queue.offer(it) }
+            }
+            result.addFirst(list)
+        }
+        return result
+    }
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
