d32f6431093fe17e2ced55122a8422fc1c8915c8
marve11ous <marve11ous@users.noreply.github.com>
2022-05-05 11:15:15 +0300

    103. Binary Tree Zigzag Level Order Traversal

---
+++ src/main/kotlin/solution/medium/n103/Solution.kt
@@ -0,0 +1,31 @@
+package solution.medium.n103
+
+import java.util.*
+
+/**
+ * [103. Binary Tree Zigzag Level Order Traversal](https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/)
+ */
+class Solution {
+    fun zigzagLevelOrder(root: TreeNode?): List<List<Int>> {
+        var reverse = false
+        val result: MutableList<List<Int>> = ArrayList()
+        val queue = LinkedList<TreeNode>().apply { root?.let { this.offer(it) } }
+        while (queue.isNotEmpty()) {
+            val list = LinkedList<Int>()
+            for (i in 1..queue.size) {
+                val node = queue.pop()
+                if (reverse) list.addFirst(node.`val`) else list.addLast(node.`val`)
+                node.left?.let { queue.offer(it) }
+                node.right?.let { queue.offer(it) }
+            }
+            result.add(list)
+            reverse = !reverse
+        }
+        return result
+    }
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
