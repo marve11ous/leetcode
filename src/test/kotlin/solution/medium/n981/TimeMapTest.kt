258a1baf08e3faf066cb3ec64a10fefd3d995ddb
marve11ous <marve11ous@users.noreply.github.com>
2022-05-10 15:09:58 +0300

    981. Time Based Key-Value Store

---
+++ src/test/kotlin/solution/medium/n981/TimeMapTest.kt
@@ -0,0 +1,27 @@
+package solution.medium.n981
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class TimeMapTest {
+
+    @Test
+    fun test() {
+        val timeMap = TimeMap()
+
+        // store the key "foo" and value "bar" along with timestamp = 1
+        timeMap.set("foo", "bar", 1)
+
+        assertEquals("bar", timeMap.get("foo", 1))
+
+        // return "bar", since there is no value corresponding to foo at timestamp 3 and timestamp 2,
+        // then the only value is at timestamp 1 is "bar"
+        assertEquals("bar", timeMap.get("foo", 3))
+
+        // store the key "foo" and value "bar2" along with timestamp = 4
+        timeMap.set("foo", "bar2", 4)
+
+        assertEquals("bar2", timeMap.get("foo", 4))
+        assertEquals("bar2", timeMap.get("foo", 5))
+    }
+}
