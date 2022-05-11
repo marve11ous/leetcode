d4aa9645562b456408dfa33842a76013603088eb
marve11ous <marve11ous@users.noreply.github.com>
2022-05-11 11:20:18 +0300

    1641. Count Sorted Vowel Strings

---
+++ src/test/kotlin/solution/medium/n1641/SolutionTest.kt
@@ -0,0 +1,25 @@
+package solution.medium.n1641
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class SolutionTest {
+
+    private val solution = Solution()
+
+    @Test
+    fun test1() {
+        assertEquals(5, solution.countVowelStrings(1))
+    }
+
+    @Test
+    fun test2() {
+        assertEquals(15, solution.countVowelStrings(2))
+    }
+
+    @Test
+    fun test3() {
+        assertEquals(66045, solution.countVowelStrings(33))
+    }
+}
