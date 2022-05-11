8548acf5503c100c57259dbc3e5aabd1f77d034b
marve11ous <marve11ous@users.noreply.github.com>
2022-05-11 15:12:47 +0300

    844. Backspace String Compare

---
+++ src/main/kotlin/solution/easy/n844/Solution.kt
@@ -0,0 +1,15 @@
+package solution.easy.n844
+
+import java.util.*
+
+/**
+ * [844. Backspace String Compare](https://leetcode.com/problems/backspace-string-compare/)
+ */
+class Solution {
+    fun backspaceCompare(s: String, t: String): Boolean {
+        return s.process() == t.process()
+    }
+
+    private fun String.process(): List<Char> =
+        LinkedList<Char>().also { this.forEach { c -> if (c != '#') it.push(c) else if (it.isNotEmpty()) it.pop() } }
+}
