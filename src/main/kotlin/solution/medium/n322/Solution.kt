3dd4cc13e1333bdf2cae73ff9c3f14cc8c4aa763
marve11ous <marve11ous@users.noreply.github.com>
2022-05-21 10:10:57 +0300

    322. Coin Change

--- src/main/kotlin/solution/medium/n322/Solution.kt
+++ src/main/kotlin/solution/medium/n322/Solution.kt
@@ -1,31 +1,29 @@
 package solution.medium.n322
 
+/**
+ * [322. Coin Change](https://leetcode.com/problems/coin-change/)
+ */
 class Solution {
     fun coinChange(coins: IntArray, amount: Int): Int {
-        if (amount < 1) {
-            return 0
-        }
-        return getCount(coins, amount, IntArray(amount))
+        return if (amount < 1) return 0 else coinChange(coins, amount, IntArray(amount))
     }
 
-    private fun getCount(coins: IntArray, amount: Int, dp: IntArray): Int {
-        if (amount == 0) {
-            return 0
-        }
-        if (amount < 0) {
-            return -1
-        }
-        if (dp[amount - 1] != 0) return dp[amount - 1]
-        var min = Int.MAX_VALUE
-        for (coin in coins) {
-            val next = getCount(coins, amount - coin, dp)
-            if (next in 0 until min) {
-                min = next + 1
+    private fun coinChange(coins: IntArray, amount: Int, dp: IntArray): Int {
+        return when {
+            amount == 0 -> 0
+            amount < 0 -> -1
+            else -> {
+                if (dp[amount - 1] == 0) {
+                    var min = Int.MAX_VALUE
+                    coins.forEach { coin ->
+                        val next = coinChange(coins, amount - coin, dp)
+                        if (next in 0 until min) min = next + 1
+                    }
+                    dp[amount - 1] = if (min == Int.MAX_VALUE) -1 else min
+                }
+                dp[amount - 1]
             }
         }
-        dp[amount - 1] = if (min == Int.MAX_VALUE) -1 else min
-        return dp[amount - 1]
     }
-
 
 }
