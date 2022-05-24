69dc7eaf106a84ec037441afac0074a2b21cefa7
marve11ous <marve11ous@users.noreply.github.com>
2022-05-24 10:49:27 +0300

    32. Longest Valid Parentheses

---
+++ src/main/kotlin/solution/hard/n32/Solution.kt
@@ -0,0 +1,21 @@
+package solution.hard.n32
+
+import java.util.*
+
+/**
+ * [32. Longest Valid Parentheses](https://leetcode.com/problems/longest-valid-parentheses/)
+ */
+class Solution {
+    fun longestValidParentheses(s: String): Int {
+        val stack = LinkedList<Int>().apply { push(-1) }
+        var result = 0
+        for (i in s.indices) {
+            if (s[i] == ')' && stack.peek() >= 0 && s[stack.pop()] == '(') {
+                result = Math.max(result, i - stack.peek())
+            } else {
+                stack.push(i)
+            }
+        }
+        return result
+    }
+}
