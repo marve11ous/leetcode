3b2680bf64d7724db33933425b718bcf3f6d1ab4
marve11ous <marve11ous@users.noreply.github.com>
2022-05-31 22:08:12 +0300

    3. Longest Substring Without Repeating Characters

--- src/main/kotlin/leetcode/solution/n3/Solution.kt
+++ src/main/kotlin/leetcode/solution/n3/Solution.kt
@@ -1,21 +1,20 @@
 package leetcode.solution.n3
 
+/**
+ * [3. Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/)
+ */
 class Solution {
 
     fun lengthOfLongestSubstring(s: String): Int {
-        val tmp: MutableMap<Char, Int> = HashMap()
-        var max = 0
+        val map = HashMap<Char, Int>()
+        var result = 0
         var i = 0
-        for (j in s.indices) {
-            if (tmp.containsKey(s[j])) {
-                i = max(tmp[s[j]], i)
-            }
-            max = max(max, j - i + 1)
-            tmp[s[j]] = j + 1
+        s.forEachIndexed { j, c ->
+            map[c]?.let { i = i.coerceAtLeast(it) }
+            result = result.coerceAtLeast(j - i + 1)
+            map[c] = j + 1
         }
-        return max
+        return result
     }
-
-    private fun max(a: Int?, b: Int): Int = if (a != null && a > b) a else b
 
 }
