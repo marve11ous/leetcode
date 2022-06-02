fe4196d31188cd55cbc9810d1cf460ae55171faf
marve11ous <marve11ous@users.noreply.github.com>
2022-06-02 13:19:01 +0300

    21. Merge Two Sorted Lists

--- src/main/kotlin/leetcode/solution/n21/Solution.kt
+++ src/main/kotlin/leetcode/solution/n21/Solution.kt
@@ -3,35 +3,28 @@ package leetcode.solution.n21
 import leetcode.util.ListNode
 
 /**
- * Example:
- * var li = ListNode(5)
- * var v = li.`val`
- * Definition for singly-linked list.
- * class ListNode(var `val`: Int) {
- *     var next: ListNode? = null
- * }
+ * [21. Merge Two Sorted Lists](https://leetcode.com/problems/merge-two-sorted-lists/)
  */
 class Solution {
     fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
-        var n1 = l1
-        var n2 = l2
-        var result: ListNode? = null
+        var node1 = l1
+        var node2 = l2
         var current: ListNode? = null
-        while (n1 != null || n2 != null) {
-            if (n1 != null && n1.`val` < n2?.`val` ?: Int.MAX_VALUE) {
-                current?.next = n1
-                current = n1
-                n1 = n1.next
-            } else if (n2 != null) {
-                current?.next = n2
-                current = n2
-                n2 = n2.next
+        var head: ListNode? = null
+        while (node1 != null || node2 != null) {
+            val next: ListNode?
+            if (node1?.`val` != null && (node2?.`val` ?: Int.MAX_VALUE) > node1.`val`) {
+                next = node1
+                node1 = node1.next
+            } else {
+                next = node2
+                node2 = node2?.next
             }
-            if (result == null) {
-                result = current
-            }
+            current?.next = next
+            current = next
+            if (head == null) head = next
         }
-        return result
+        return head
     }
 
 }
