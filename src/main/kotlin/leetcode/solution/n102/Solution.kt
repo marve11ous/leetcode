4605921bff38693dcacde3c0851d1da9cc4485cc
marve11ous <marve11ous@users.noreply.github.com>
2022-06-05 18:15:04 +0300

    102. Binary Tree Level Order Traversal

--- src/main/kotlin/leetcode/solution/n102/Solution.kt
+++ src/main/kotlin/leetcode/solution/n102/Solution.kt
@@ -1,28 +1,27 @@
 package leetcode.solution.n102
 
 import leetcode.util.TreeNode
+import java.util.*
 
 /**
- * 102. Binary Tree Level Order Traversal
- *
- * Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ * [102. Binary Tree Level Order Traversal](https://leetcode.com/problems/binary-tree-level-order-traversal/)
  */
 class Solution {
     fun levelOrder(root: TreeNode?): List<List<Int>> {
-        val result = ArrayList<ArrayList<Int>>()
-        levelOrder(root, 0, result)
+        val queue = LinkedList<TreeNode>()
+        if (root != null) queue.offer(root)
+        val result = ArrayList<List<Int>>()
+        while (queue.isNotEmpty()) {
+            var size = queue.size
+            val level = ArrayList<Int>(size)
+            while (size-- > 0) {
+                val node = queue.poll()
+                level.add(node.`val`)
+                node.left?.let { queue.offer(it) }
+                node.right?.let { queue.offer(it) }
+            }
+            result.add(level)
+        }
         return result
-    }
-
-    private fun levelOrder(root: TreeNode?, depth: Int, result: ArrayList<ArrayList<Int>>) {
-        if (root == null) {
-            return
-        }
-        if (result.size <= depth) {
-            result.add(ArrayList())
-        }
-        result[depth].add(root.`val`)
-        levelOrder(root.left, depth + 1, result)
-        levelOrder(root.right, depth + 1, result)
     }
 }
