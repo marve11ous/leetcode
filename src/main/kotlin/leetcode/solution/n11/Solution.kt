2b83452499036f5f879b58d65c53978641b04739
marve11ous <marve11ous@users.noreply.github.com>
2022-06-07 15:46:54 +0300

    11. Container With Most Water

--- src/main/kotlin/leetcode/solution/n11/Solution.kt
+++ src/main/kotlin/leetcode/solution/n11/Solution.kt
@@ -1,23 +1,19 @@
 package leetcode.solution.n11
 
+/**
+ * [11. Container With Most Water](https://leetcode.com/problems/container-with-most-water/)
+ */
 class Solution {
     fun maxArea(height: IntArray): Int {
+        var l = 0
+        var r = height.size - 1
         var result = 0
-        var left = 0
-        var right = height.size - 1
-        while (left < right) {
-            val s = min(height[left], height[right]) * (right - left)
-            if (result < s) {
-                result = s
-            }
-            if (height[left] < height[right]) {
-                left++
-            } else {
-                right--
-            }
+        while (l < r) {
+            val width = r - l
+            val minHeight = if (height[l] < height[r]) height[l++] else height[r--]
+            result = result.coerceAtLeast(minHeight * width)
         }
         return result
     }
 
-    private fun min(a: Int, b: Int) = if (a < b) a else b
 }
