ecd1ad460bd82e256a332ad26e7aaca304d6a4ff
marve11ous <15189957+marve11ous@users.noreply.github.com>
2022-06-29 15:44:06 +0200

    1091. Shortest Path in Binary Matrix

--- src/main/kotlin/leetcode/solution/n1091/Solution.kt
+++ src/main/kotlin/leetcode/solution/n1091/Solution.kt
@@ -6,30 +6,27 @@ import java.util.*
  * [1091. Shortest Path in Binary Matrix](https://leetcode.com/problems/shortest-path-in-binary-matrix/)
  */
 class Solution {
+    private companion object {
+        private val directions = listOf(1 to 0, 1 to 1, 1 to -1, 0 to 0, 0 to 1, 0 to -1, -1 to 0, -1 to 1, -1 to -1)
+    }
 
     fun shortestPathBinaryMatrix(grid: Array<IntArray>): Int {
         val queue = LinkedList<Pair<Int, Int>>()
-        if (grid[0][0] == 0 && grid[grid.size - 1][grid.size - 1] == 0) queue.offer(0 to 0)
+        val n = grid.size - 1
+        if (grid[0][0] == 0 && grid[n][n] == 0) queue.offer(0 to 0)
         var length = 1
         while (queue.isNotEmpty()) {
             var size = queue.size
             while (size-- > 0) {
                 val (i, j) = queue.pop()
-                if (i == grid.size - 1 && j == i) {
-                    return length
-                }
-                listOf(
-                    i - 1 to j,
-                    i - 1 to j + 1,
-                    i - 1 to j - 1,
-                    i to j + 1,
-                    i to j - 1,
-                    i + 1 to j,
-                    i + 1 to j + 1,
-                    i + 1 to j - 1
-                )
-                    .filter { it.first in grid.indices && it.second in grid.indices && grid[it.first][it.second] == 0 }
-                    .forEach { grid[it.first][it.second] = 1; queue.offer(it) }
+                if (i == n && j == n) return length
+                directions
+                    .map { i + it.first to j + it.second }
+                    .filter { (r, c) -> r in grid.indices && c in grid.indices && grid[r][c] == 0 }
+                    .forEach {
+                        grid[it.first][it.second] = 1
+                        queue.offer(it)
+                    }
             }
             length++
         }
