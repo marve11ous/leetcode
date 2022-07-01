d136c422ee394d267fd0f75e3dc2e7014c60fd07
marve11ous <15189957+marve11ous@users.noreply.github.com>
2022-07-01 13:10:05 +0200

    47. Permutations II

--- src/main/kotlin/leetcode/solution/n47/Solution.kt
+++ src/main/kotlin/leetcode/solution/n47/Solution.kt
@@ -1,20 +1,32 @@
 package leetcode.solution.n47
 
+import java.util.LinkedList
+
 /**
  * [47. Permutations II](https://leetcode.com/problems/permutations-ii/)
  */
 class Solution {
     fun permuteUnique(nums: IntArray): List<List<Int>> {
         val result = ArrayList<List<Int>>()
-        permuteUnique(result, nums.size, listOf(), nums.toList().groupingBy { it }.eachCount())
+        val counts = IntArray(21)
+        nums.forEach { counts[it + 10]++ }
+        permuteUnique(result, nums.size, LinkedList(), counts)
         return result
     }
 
-    private fun permuteUnique(result: ArrayList<List<Int>>, n: Int, current: List<Int>, counts: Map<Int, Int>) {
-        if (current.size == n) result += current
-        else counts.forEach { permuteUnique(result, n, current + it.key, counts.decrement(it.key)) }
+    private fun permuteUnique(result: ArrayList<List<Int>>, n: Int, current: LinkedList<Int>, counts: IntArray) {
+        if (current.size == n) {
+            result += ArrayList(current)
+            return
+        }
+        for (i in counts.indices) {
+            if (counts[i] > 0) {
+                counts[i]--
+                current.addLast(i - 10)
+                permuteUnique(result, n, current, counts)
+                current.removeLast()
+                counts[i]++
+            }
+        }
     }
-
-    private fun Map<Int, Int>.decrement(num: Int): Map<Int, Int> =
-        toMutableMap().apply { compute(num) { _, v -> if (v != null && v > 1) (v - 1) else null } }
 }
